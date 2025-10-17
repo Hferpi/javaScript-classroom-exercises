285e43931f46ef17d5c9c0d8b193dc17ed250069
«hferpi» <hferpi@gmail.com>
2025-10-17 16:18:25 +0200

    modificar ruta

--- PROGRAMACIÓN AVANZADA 2.0 (BOM)/BOM_1/script.js
+++ PROGRAMACIÓN AVANZADA 2.0 (BOM)/BOM_1/script.js
@@ -4,7 +4,7 @@ let currentNavEntries = window.name;
 console.log(currentNavEntries)
 
 let avanzar = confirm("Tu lenguaje del navegador es: "+lenguajeNavegador)
-let siguiente = window.open("http://127.0.0.1:5500/BOM_1/siguiente_pagina.html")
+let siguiente = window.open("siguiente_pagina.html")
 
 avanzar ? (siguiente) : console.log("Cerrar")
 
